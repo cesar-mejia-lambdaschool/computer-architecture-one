c3db7d04e0a088c49d44cb6e3eaeddd3a10bfe5d
Cesar Mejia Leiva <cmejia@gmail.com>
2018-05-14 15:19:35 -0700

    working on tick; PP w/ AD

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -6,43 +6,42 @@
  * Class for simulating a simple Computer (CPU & memory)
  */
 class CPU {
-
-    /**
+  /**
      * Initialize the CPU
      */
-    constructor(ram) {
-        this.ram = ram;
+  constructor (ram) {
+    this.ram = ram
 
-        this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
-        
-        // Special-purpose registers
-        this.PC = 0; // Program Counter
-    }
-    
-    /**
+    this.reg = new Array(8).fill(0) // General-purpose registers R0-R7
+
+    // Special-purpose registers
+    this.PC = 0 // Program Counter
+  }
+
+  /**
      * Store value in memory address, useful for program loading
      */
-    poke(address, value) {
-        this.ram.write(address, value);
-    }
+  poke (address, value) {
+    this.ram.write(address, value)
+  }
 
-    /**
+  /**
      * Starts the clock ticking on the CPU
      */
-    startClock() {
-        this.clock = setInterval(() => {
-            this.tick();
-        }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
-    }
+  startClock () {
+    this.clock = setInterval(() => {
+      this.tick()
+    }, 1) // 1 ms delay == 1 KHz clock == 0.000001 GHz
+  }
 
-    /**
+  /**
      * Stops the clock
      */
-    stopClock() {
-        clearInterval(this.clock);
-    }
+  stopClock () {
+    clearInterval(this.clock)
+  }
 
-    /**
+  /**
      * ALU functionality
      *
      * The ALU is responsible for math and comparisons.
@@ -52,45 +51,61 @@ class CPU {
      *
      * op can be: ADD SUB MUL DIV INC DEC CMP
      */
-    alu(op, regA, regB) {
-        switch (op) {
-            case 'MUL':
-                // !!! IMPLEMENT ME
-                break;
-        }
+  alu (op, regA, regB) {
+    switch (op) {
+      case 'MUL':
+        // !!! IMPLEMENT ME
+        break
     }
+  }
 
-    /**
+  /**
      * Advances the CPU one cycle
      */
-    tick() {
-        // Load the instruction register (IR--can just be a local variable here)
-        // from the memory address pointed to by the PC. (I.e. the PC holds the
-        // index into memory of the instruction that's about to be executed
-        // right now.)
+  tick () {
+    // Load the instruction register (IR--can just be a local variable here)
+    // from the memory address pointed to by the PC. (I.e. the PC holds the
+    // index into memory of the instruction that's about to be executed
+    // right now.)
+    let IR = this.ram.mem[this.PC]
+    // console.log(IR, this.PC, 'ir, this.PC')
 
-        // !!! IMPLEMENT ME
+    // !!! IMPLEMENT ME
 
-        // Debugging output
-        //console.log(`${this.PC}: ${IR.toString(2)}`);
+    // Debugging output
+    // console.log(`${this.PC}: ${IR.toString(2)}`)
 
-        // Get the two bytes in memory _after_ the PC in case the instruction
-        // needs them.
+    // Get the two bytes in memory _after_ the PC in case the instruction
+    // needs them.
 
-        // !!! IMPLEMENT ME
+    const operandA = this.ram.mem[this.PC + 1]
+    const operandB = this.ram.mem[this.PC + 2]
 
-        // Execute the instruction. Perform the actions for the instruction as
-        // outlined in the LS-8 spec.
+    // console.log(
+    //   'instructions+2 arguments',
+    //   IR.toString(2),
+    //   inst1.toString(2),
+    //   inst2.toString(2)
+    // )
 
-        // !!! IMPLEMENT ME
+    // !!! IMPLEMENT ME
+    switch(IR) {
+      case 'LDI':
 
-        // Increment the PC register to go to the next instruction. Instructions
-        // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
-        // instruction byte tells you how many bytes follow the instruction byte
-        // for any particular instruction.
-        
-        // !!! IMPLEMENT ME
+      break
     }
+    // Execute the instruction. Perform the actions for the instruction as
+    // outlined in the LS-8 spec.
+
+    // !!! IMPLEMENT ME
+
+    // Increment the PC register to go to the next instruction. Instructions
+    // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
+    // instruction byte tells you how many bytes follow the instruction byte
+    // for any particular instruction.
+    this.PC = this.PC + 
+    // !!! IMPLEMENT ME
+  }
 }
 
-module.exports = CPU;
+module.exports = CPU
